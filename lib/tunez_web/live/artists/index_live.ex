525b3e87ce7c8257af92894780c493830fd1fc01
Alexandre Hamez <alexandre.hamez@gmail.com>
2025-04-10 14:33:13 +0200

    Move artist search load options to default configuration

--- lib/tunez_web/live/artists/index_live.ex
+++ lib/tunez_web/live/artists/index_live.ex
@@ -29,7 +29,6 @@ defmodule TunezWeb.Artists.IndexLive do
       Tunez.Music.search_artists!(query_text,
         page: artists_page_params,
         query: [sort_input: sort_by],
-        load: [:album_count, :latest_album_year_released, :cover_image_url],
         actor: socket.assigns.current_user
       )
 
